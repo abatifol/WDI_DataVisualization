176b8549cc19634fd0efe39ca93c82644fe9d956
Antonine Batifol <antonine.batifol@student-cs.fr>
2025-04-09 20:51:04 +0200

    global view financial aid flows

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    df_2021 = pd.read_csv('./data/oda_2021.csv')
+    # df_2021 = pd.read_csv('./data/oda_2021.csv')
+    oda = pd.read_parquet('./data/oda_disbursment.parquet')
     # df_2021 = data[data.Year == 2021].dropna(subset=['Value'])
     print('ODA data loaded')
     countries = pd.read_csv('./data/WDICountry.csv').rename(columns={"Country Code":"DE_code"})
@@ -32,9 +33,9 @@ def load_data():
     correspondance = pd.read_csv('./data/Correspondences_DAC2a.csv')
     print('correspondance data loaded')
     poverty = pd.read_csv('data/oda_poverty_world.csv')
-    return df_2021, countries, correspondance, poverty
+    return oda, countries, correspondance, poverty
 
-df_2021, countries, correspondance, poverty = load_data()
+oda, countries, correspondance, poverty = load_data()
 
 #################################################
 ## --------------- Intro charts -------------- ##
@@ -132,14 +133,91 @@ chart2 = alt.Chart(df_pov_regions[(df_pov_regions.Year >= 1990) & (df_pov_region
     height=500
 )
 
+
+
+#########################################################
+# --- Part 3: Financial Flows and Poverty Reduction --- #
+#########################################################
+
+# -- - Chart 3: Financial Flows to the World ---
+world_financial_aid_flows = poverty[(poverty['Country Name']=='World') & (poverty['Year'] >= 1990) & (poverty['Year'] <= 2022)]
+world_financial_aid_flows = world_financial_aid_flows[['Country Name','Year',"Net official development assistance received (current US$)",
+'Foreign direct investment, net inflows (BoP, current US$)','Personal remittances, received (current US$)']]
+world_financial_aid_flows = pd.melt(world_financial_aid_flows, id_vars=['Country Name','Year'], var_name='Indicator', value_name='Value')
+world_financial_aid_flows.loc[:,'Value_M'] = world_financial_aid_flows.loc[:,'Value'] / 1000_000_000
+world_financial_aid = alt.Chart(world_financial_aid_flows).mark_line().encode(
+    x='Year:O',
+    y=alt.Y('Value_M:Q',title='Flow in Billion US$'),
+    color=alt.Color('Indicator:N', legend=alt.Legend(
+        columns=1,
+        orient='bottom',  
+        title='Financial Aid Indicators',
+        labelLimit=500,  
+        direction='vertical',
+        symbolSize=150
+    )),
+    tooltip=['Year:O', 'Indicator:N', 'Value_M:Q']
+).properties(
+    width=500,
+    height=500,
+    title='Global Financial flows Over Time'
+).interactive()
+
+
+# -- Chart 4: Different Types of Aid Over Time and Recipients --
+aid_types = ['Imputed Multilateral ODA', 'Technical Cooperation', 'Development Food Aid', 'Humanitarian Aid']
+
+# recipients =  
+recipients = ['All Recipients, Total','Western Africa, Total','South of Sahara, Total',
+ 'Southern Africa, Total', 'South & Central Asia, Total','South America, Total','Oceania, Total',
+'North of Sahara, Total', 'Northern America, Total','Middle East, Total','Fragile states, Total',
+'Europe, Total','Eastern Africa, Total','Developing Countries, Total','Caribbean & Central America, Total',
+'Asia, Total','All Recipients, Total','Africa, Total','LDCs, Total',
+ 'LMICs, Total']
+
+recipient_dropdown = alt.selection_point(
+    fields=['Recipient'],
+    bind=alt.binding_select(options=sorted(recipients), name='Select a Recipient: '),
+    value=[{'Recipient':'All Recipients, Total'}]
+)
+
+# recipients_dropdown
+oda_world = oda[
+    (oda['Recipient'].isin(recipients)) & 
+    (oda['Year'] >= 1990) & 
+    (oda['Year'] <= 2022) & 
+    (oda['Aid type'].isin(aid_types))
+]
+
+# Aggregate and convert to billion $
+oda_agg = oda_world.groupby(['Year', 'Aid type','Recipient'], as_index=False)['Value'].sum()
+oda_agg['Value_k'] = oda_agg['Value'] / 1000.0
+
+world_aid_type = alt.Chart(oda_agg).mark_line().encode(
+    x='Year:O',
+    y=alt.Y('Value_k:Q',title='Flow in million US$'),
+    color=alt.Color('Aid type:N'),
+    tooltip=['Year:O', 'Aid type:N', 'Value_k:Q']
+).interactive().properties(
+    width=200,
+    height=400,
+    title='Evolution of Different Types of Aid Over Time by Region'
+).add_params(
+    recipient_dropdown
+).transform_filter(
+    recipient_dropdown
+)
+
 #################################################
 ## ------------ Streamlit Display ------------ ##
 #################################################
 
 st.title("🌍 How economic growth and financial aid shape the fight against poverty?")
 
+##############################################
 # --- Part 1: Overview of Global Poverty --- #
-st.subheader("Part 1: Global Overview: Population Growth & Poverty Reduction")
+##############################################
+st.header("Part 1: Global Overview: Population Growth & Poverty Reduction")
 
 # Add top-level indicators
 latest_year = 2022
@@ -171,6 +249,72 @@ with col2:
 
 st.markdown("**Note:** The number of extreme poor is calculated using the poverty headcount ratio at $2.15 a day (2017 PPP) and the total population.")
 
+#############################################
 # --- Part 2: GDP and Poverty Reduction --- #
+#############################################
 
+
+
+
+
+#########################################################
 # --- Part 3: Financial Flows and Poverty Reduction --- #
+#########################################################
+
+st.markdown(""" Economic growth alone doesn't explain everything. Let's dig deeper into the role of international financial flows,
+such as aid, investments, and remittances, in shaping poverty outcomes worldwide.""")
+st.header("Part 2: Understanding financial flows and their impact on poverty reduction")
+
+st.subheader("Section 1: Global Financial Flows — The Big Picture")
+st.markdown("""To understand how countries combat poverty, it is interesting to look at the financial
+             lifelines flowing across borders like:
+            - **Net Official Development Assistance (ODA):** Grants or concessional loans from government to support development.
+            - **Foreign Direct Investment (FDI):** Cross-border investments, indicating private sector engagement.
+            - **Personal Remittances:** Funds sent by individuals (often migrant workers) to their home countries.
+.""")
+
+# How have different financial flows evolved over time?
+# Are we seeing a growing reliance on private flows (remittances, FDI) versus official aid?
+# Does global aid increase in times of crisis?
+
+st.altair_chart(world_financial_aid, use_container_width=True)
+st.markdown(
+'''
+### Key Observations:
+- **Foreign Direct Investment (FDI)** has become the **dominant source of external finance** over the years. Unlike aid, FDI often targets infrastructure, industries, and services that stimulate long-term economic growth and employment creation.
+- **Remittances** have experienced a **remarkable surge**, outpacing the growth of traditional aid flows.  
+  This growth reflects both the increase in migration and the resilience of remittance flows during crises.  
+  For example, during economic downturns or disasters, remittances tend to remain stable or even increase, as migrants support their families back home.
+- **Official Development Assistance (ODA)** remains vital, especially for countries with limited access to private finance, but its relative share has decreased compared to private flows.
+
+*Overall, this shift highlights how global development financing has diversified beyond aid, with private sector flows and diaspora remittances playing increasingly important roles.*
+
+---
+
+# Aid Types Evolution by Region (1990–2022)
+''')
+
+
+st.altair_chart(world_aid_type, use_container_width=True)
+
+st.markdown('''
+
+A notable trend emerges when observing the evolution of aid types: **Imputed Multilateral ODA** has steadily increased since the late 1990s, surpassing other aid types to become the largest component of aid flows by the early 2000s. 
+
+This growth reflects the **increasing importance of multilateral institutions** in the global development landscape. Countries have progressively channeled more resources through organizations like the **United Nations**, **World Bank**, and **regional development banks**, recognizing the need for coordinated, impartial, and large-scale responses to complex global challenges.
+
+### Regional Disparities & Notable Events:
+
+- **Caribbean & Central America (2010):**  
+  A dramatic spike in **Humanitarian Aid** corresponds with the devastating earthquake in **Haiti** in January 2010.  
+  The international community responded with unprecedented humanitarian assistance to support emergency relief and reconstruction efforts.
+            
+- **Middle East:**  
+  Periodic increases in **Humanitarian Aid** can be linked to conflicts and refugee crises in the region, such as the Syrian conflict starting in 2011.
+
+*This chart emphasizes how aid flows respond dynamically to regional crises, geopolitical events, and development priorities.*
+
+End **reflection**
+   - "How might future aid and private flows evolve as global challenges like climate change and pandemics shape development priorities?"
+'''
+)
