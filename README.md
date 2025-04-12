69b6e91990798108aa4fbf447f4fc602345c44d6
Antonine Batifol <antonine.batifol@student-cs.fr>
2025-04-12 20:23:01 +0200

    add readme and preprocessing code

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# WDI_DataVisualization
+# 🌍 Interactive Dashboard for International Aid & Economic Development
+
+## Project Overview
+
+This project is part of the Data Visualization (2025) class in CentraleSupélec. It explores **SDG 1: No Poverty** and **SDG 17: Partnership for the goals** by visualizing the relationship between **economic growth**, **international aid flows** and poverty. Using data from the **World Bank** and **OECD**, we built an **interactive dashboard** that helps users understand how different countries and regions contribute to global poverty reduction through **Official Development Assistance (ODA)**.
+
+It is built with **Streamlit**, **Altair**, and **Plotly** for dynamic visualizations.
+
+## Data Sources
+
+- **World Bank — World Development Indicators (WDI):**  
+  *https://datacatalog.worldbank.org/search/dataset/0037712*
+
+- **OECD — DAC2A Official Development Assistance (ODA) flow:**  
+  *https://data-explorer.oecd.org/vis?lc=en&df[ds]=dsDisseminateFinalDMZ&df[id]=DSD_DAC2%40DF_DAC2A&df[ag]=OECD.DCD.FSD&df[vs]=1.3&dq=....&lom=LASTNPERIODS&lo=5&to[TIME_PERIOD]=false*
+
+
+## Installation & Setup
+
+Follow these steps to run the project locally:
+
+### 1. Clone the Repository
+
+```bash
+git clone https://github.com/abatifol/WDI_DataVisualization.git
+cd WDI_DataVisualization
+```
+
+### 2. Install Dependencies
+
+Make sure you have Python installed and install the required packages:
+
+```bash
+pip install -r requirements.txt
+```
+
+### 3. Run the Application
+
+Start the Streamlit app with:
+
+```bash
+streamlit run app.py
+```
+
+Your browser will open automatically with the dashboard!
+
+
+## Contributors
+
+- **Antonine Batifol**
+- **William Slimi**
+
+
+## Limitations
+
+- Current data may not include the latest data since there is no real-time update.
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,33 +1,96 @@
 import pandas as pd
-import streamlit as st
 
 
-# We load and extract relevant data from the dataset beforehand to create a smaller dataset
-# to make the streamlit app faster and more efficient.
-
-data = pd.read_csv('../Assignements/WDI_CSV_2025_01_28/WDICSV.csv')
-countries = pd.read_csv('../Assignements/WDI_CSV_2025_01_28/WDICountry.csv')
+# Load and preprocess the data from the World Bank
+data = pd.read_csv('../WDI_CSV_2025_01_28/WDICSV.csv')
+countries = pd.read_csv('../WDI_CSV_2025_01_28/WDICountry.csv')
 df = pd.melt(data, id_vars=['Country Name', 'Country Code', 'Indicator Name',
                 'Indicator Code'], var_name='Year', value_name='Value')
 df = df.merge(countries[['Country Code', 'Region',
                 'Income Group']], on='Country Code', how='left')
 df['Year'] = df['Year'].astype(int)
 
-indicators = [
-    'Poverty gap at $2.15 a day (2017 PPP) (%)',
-    'Poverty gap at $3.65 a day (2017 PPP) (%)',
-    'Poverty gap at $6.85 a day (2017 PPP) (%)',
-    'Poverty headcount ratio at $2.15 a day (2017 PPP) (% of population)',
-    'Poverty headcount ratio at $3.65 a day (2017 PPP) (% of population)',
-    'Poverty headcount ratio at $6.85 a day (2017 PPP) (% of population)',
-    'Poverty headcount ratio at national poverty lines (% of population)',
-    'Poverty headcount ratio at societal poverty line (% of population)',
-    'Multidimensional poverty headcount ratio (World Bank) (% of population)',
-    'Population, total',
-    'GDP per capita (constant 2015 US$)',
-    'GNI per capita (constant 2015 US$)'
-]
 
+# Filter indicators and regions
+indicators = [
+    "Net ODA provided, to the least developed countries (current US$)",
+    "Net ODA provided, total (% of GNI)",
+    "Net ODA provided, total (constant 2021 US$)",
+    "Net ODA provided, total (current US$)",
+    "Net ODA received (% of central government expense)",
+    "Net ODA received (% of GNI)",
+    "Net ODA received per capita (current US$)",
+    "Net official development assistance received (constant 2021 US$)",
+    "Net official development assistance received (current US$)",
+    "Poverty headcount ratio at $2.15 a day (2017 PPP) (% of population)",
+    "Population, total",
+    "Personal remittances, received (current US$)",
+    "Foreign direct investment, net inflows (BoP, current US$)",
+    "GNI (current US$)","GDP per capita (current US$)",
+    "GDP (current US$)"
+]
 df = df[df['Indicator Name'].isin(indicators)]
 
-df.to_csv('data/poverty_data.csv', index=False)
+# # Pivot and clean
+df = pd.pivot_table(df, 
+                    index=['Country Name','Country Code', 'Year'],
+                    columns='Indicator Name', 
+                    values='Value').reset_index()
+
+df.columns = [col.replace(' (2017 PPP) (% of population)', '') for col in df.columns]
+df['Year'] = df['Year'].astype(int)
+df = df[df.Year >=1990]
+df.to_parquet('./data/oda_poverty_world.parquet', engine='pyarrow', index=False)
+
+countries = pd.read_csv('../WDI_CSV_2025_01_28/WDICountry.csv')
+countries  = countries[['Country Code','Region','Income Group']]
+countries.to_parquet('./data/WDICountry.parquet', engine='pyarrow',index=False)
+
+
+# Load and preprocess the data from the OECD
+oda = pd.read_csv('../data_csv/ODA_disbursment.csv')
+countries = pd.read_csv('../WDI_CSV_2025_01_28/WDICountry.csv')
+correspondance = pd.read_csv('../data_csv/Correspondences_DAC2a.csv')
+oda  = oda[(oda.Year >= 1990) & (oda.Year <= 2022) & (oda['Amount type']=='Constant Prices (2022 USD millions)')]
+countries = countries.rename(columns={'Country Code':'DE_code'})
+correspondance = correspondance.merge(countries[['DE_code','Region']]).drop(columns='DE_code')
+correspondance['dotstat_code']=correspondance['dotstat_code'].astype(int)
+oda = oda.merge(correspondance.rename(columns={'dotstat_code':'RECIPIENT','Region':'Recipient_Region'}), how='left')
+oda = oda.merge(correspondance.rename(columns={'dotstat_code':'DONOR','Region':'Donor_Region'}), how='left')
+oda = oda[oda['Aid type'].isin(['Imputed Multilateral ODA', 'ODA: Total Net', 'Technical Cooperation','Memo: ODA Total, Gross disbursements',
+       'ODA as % GNI (Recipient)','ODA per Capita', 'Development Food Aid', 'Humanitarian Aid'])]
+oda.to_parquet('./data/oda_disbursment.parquet', engine='pyarrow', index=False)
+
+
+# prepare donor and recipient dataframes to display top 10 recipients/donor and group the rest in Other for each donor/recipient
+oda_filtered = oda[oda.Recipient_Region.notna() & oda.Donor_Region.notna()].copy()
+donor_recipient = (
+    oda_filtered
+    .groupby(['Recipient', 'Donor'], as_index=False)['Value']
+    .sum()
+)
+donor_recipient['Rank'] = donor_recipient.groupby('Donor')['Value'].rank(method='first', ascending=False)
+donor_recipient['Recipient_Group'] = donor_recipient['Recipient'].where(donor_recipient['Rank'] <= 10, 'Others')
+df_prepared = (
+    donor_recipient
+    .groupby(['Donor', 'Recipient_Group'], as_index=False)['Value']
+    .sum()
+)
+donor_prepared = df_prepared.rename(columns={'Recipient_Group':'Recipient'})
+donor_prepared.to_parquet("./data/chart8_donor_prepared.parquet",engine="pyarrow", index=False)
+
+oda_filtered = oda[oda.Recipient_Region.notna() & oda.Donor_Region.notna()].copy()
+donor_recipient = (
+    oda_filtered
+    .groupby(['Recipient', 'Donor'], as_index=False)['Value']
+    .sum()
+)
+donor_recipient['Rank'] = donor_recipient.groupby('Recipient')['Value'].rank(method='first', ascending=False)
+donor_recipient['Donor_Group'] = donor_recipient['Donor'].where(donor_recipient['Rank'] <= 10, 'Others')
+df_prepared = (
+    donor_recipient
+    .groupby(['Recipient', 'Donor_Group'], as_index=False)['Value']
+    .sum()
+)
+recipient_prepared = df_prepared.rename(columns={'Donor_Group':'Donor'})
+recipient_prepared.to_parquet("./data/chart8_recipient_prepared.parquet",engine="pyarrow", index=False)
